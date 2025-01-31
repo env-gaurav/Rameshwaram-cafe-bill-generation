b435cce3e03f01c8afeed7c84070ce855d3cb1cd
env-gaurav <gaurav2528singh@gmail.com>
2025-01-31 12:07:48 +0530

    rameshwaram cafe bill generation

---
+++ rameshwaram.py
@@ -0,0 +1,37 @@
+cafe = '''
+          THE RAMESHWARAM CAFE
+          #1 Green Avenue , ITPM Road,
+          Banglore
+          PIN :560037
+          29ABYFA2044B1ZG
+          
+----------------------------------------
+NAME
+----------------------------------------
+Date: 24/12/24   Self Service: SS
+20;38
+Cashier:Devika         Bill No:
+                      RC/00413707
+Token No:
+1107
+---------------------------------------
+No Item     QTY       Price    Amount
+GHEE PUDI    2         128      256
+IDLI         2         85       170
+PLAIN IDLI   1         38       38
+---------------------------------------
+TOTAL QTY    5    SUB TOTAL 251 , 464
+
+                  CGST 2.5%       11.67
+                  SGST 2.5%       11.67
+----------------------------------------
+             GRAND TOTAL          RS 487
+-----------------------------------------
+FSSAI Lic N0.  11222333001923
+           THANK YOU VISIT AGAIN
+           
+   BEST TO CONSUME THE FOOD WITHIN 4 MINS
+         FROM THE BILLING TIME
+
+'''
+print(cafe)
